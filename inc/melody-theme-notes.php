237c5d87f8a4bc1474b67b0928c2c997b9865e28
Larry <tradesouthwest@gmail.com>
2023-12-02 10:19:35 -0700

    two new widgets

---
+++ inc/melody-theme-notes.php
@@ -0,0 +1,42 @@
+
+<header>
+        <h2><?php esc_html_e( 'Theme Help', 'melody' ); ?></h2>
+        <h5><?php esc_html_e( 'Melody is a general purpose theme made for ClassicPress. Please Enjoy Fully!', 'melody' ); ?></h5>
+        </header>
+        <article>
+<ul class="melody-theme-notes">
+<li id="aRelNote"><h3><?php esc_html_e( 'Theme Setup', 'melody' ); ?></h3>
+</li>
+<li>Melody is designed to approach a concept on how to make page building a more user-friendly and SEO (page speed load time) friendly process. 
+    Page builders take up a lot of bandwidth and are sometimes filled with lots of excessive code that just slows down your website. 
+    So we attempt to eleminate over-bloat of code by using built in components of WordPress that are already part of its core functionality. 
+
+    We use widgets to allow you to inset just about any content type into the lower part of your normal content. Curently there is a limit to the 
+    number of available widgets that come with Melody but we will add sectional widgets---as a plugin---as time goes by.
+</li>
+<li>Tips:</li>
+<li>a. You can use the Excerpt option in your content editor by enabling the "Screen Options" checkbox at the top of the editor. This gives you yet another manageable spot to add content.<sup>[1]</sup></li>
+<li>Tips:</li>
+<li>Tips:</li>
+<li>Tips:</li>
+</ul>
+<ul class="subz">
+<li ><sub>1</sub> Excerpts can be added to "pages" as well as regular posts. this can only be done on the Landing page template and not on other pages. This gives you an extended section to work with that you could change on a periodical basis so you can add announcements. Or use it to add plain text and bits of HTML to the lower section of the page.</li>
+<li ><sub>2</sub> </li>
+</ul>
+<ul class="melody-theme-notes">
+<li id="aRelNote"><h3><?php esc_html_e( 'Landing-page Setup', 'melody' ); ?></h3>
+</li>
+</ul>
+
+        <footer>
+        <p><?php esc_html_e('Theme created by Tradesouthwest USA... Support can be found at ', 'melody' ); ?>
+        <a href="https://themes.tradesouthwest.com Opens in new window" target="_blank" 
+            title="https://themes.tradesouthwest.com">
+        <?php echo esc_attr( "https://themes.tradesouthwest.com" ); ?></a></p>
+        </footer>
+</article>
+<style>.melody-theme-notes li{border-bottom: thin solid #ddd;}.melody-theme-notes li p{margin: 0}
+        .melody-theme-notes li strong{font-size: inherit; border: thin solid #ccc; padding:3px 3px 0;margin-right: 3px;background: #fafaf0;}
+    .melody-theme-notes li strong:after{content: ": ";}.melody-theme-notes h3{background: #fcfcfc;padding: 2px 3px}
+    .subz li{font-size: smaller}</style>
